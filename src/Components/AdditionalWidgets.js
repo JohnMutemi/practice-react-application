7bba947b62739f34d54cc1b3d79f6012992a6f96
JohnMutemi <jmtutorsalp@gmail.com>
2024-05-17 17:08:49 +0300

    commit additional widgets

--- src/Components/AdditionalWidgets.js
+++ src/Components/AdditionalWidgets.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
-export default function AdditionalWidgets() {
-  return <div>AdditionalWidgets</div>;
+function AdditionalWidgets({ matchSchedule, teamNews }) {
+  return (
+    <div className="additional-widgets">
+      {/* Match Schedule */}
+      <div className="widget">
+        <h3>Match Schedule</h3>
+        <ul>
+          {matchSchedule.map((match) => (
+            <li key={match.id}>
+              <span>{match.date}</span> - {match.opponent}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Team News */}
+      <div className="widget">
+        <h3>Team News</h3>
+        <ul>
+          {teamNews.map((news) => (
+            <li key={news.id}>
+              <span>{news.date}</span> - {news.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+export default AdditionalWidgets;
